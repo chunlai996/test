66f615d5739d15e405c04b013bc145abe9e4c12f
chunlai <a565354062@pangxiaoyandeMacBook-Pro.local>
2020-07-17 15:45:13 +0800

    test

--- src/main/java/cn/chunlai/learngit/App.java
+++ src/main/java/cn/chunlai/learngit/App.java
@@ -4,10 +4,8 @@ package cn.chunlai.learngit;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App {
+    public static void main( String[] args ){
         System.out.println( "Hello World!" );
     }
 }
